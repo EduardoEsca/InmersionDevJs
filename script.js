ef8f34a970cfb763d3a38682b1baf429c934889c
EduardoEsca <eduescadev@gmail.com>
2023-11-07 19:10:58 -0600

    Fifth commit

---
+++ script.js
@@ -0,0 +1,11 @@
+function redirigirConversor () {
+    window.location.href = 'Conversor monedas/index.html';
+}
+
+function redirigirMentalista () {
+    window.location.href = 'Mentalista/index.html';
+}
+
+function redirigirCatalogo () {
+    window.location.href = 'Catalogo Videojuegos/index.html';
+}
